717924967257694e8ca1cf65aeb6b6ec3f00378d
Dev-Govind-10 <govindprajapati6660@gmail.com>
2025-10-03 13:57:58 +0530

    Update writeFile.py

--- Assignment2/writeFile.py
+++ Assignment2/writeFile.py
@@ -1,9 +1,12 @@
+# Write a file
 f = open('myFile.txt', 'w')
 
 f.write('Hi this is the assignment2.\nDoing the exercise for the devOps')
 f.close()
 
+#Read a file
 f = open('myFile.txt', 'r')
 
 data = f.read()
 print(data)
+f.close()
